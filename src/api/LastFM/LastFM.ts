f31e7adb8ab48acdff82cccb55e1dafe6f880d81
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-29 19:41:36 +0000

    Add API validation, and undefined handling

--- src/api/LastFM/LastFM.ts
+++ src/api/LastFM/LastFM.ts
@@ -15,6 +15,8 @@ type ArtistData = {
   threshold: Number,
 }
 
+// we can have multiple users defined in the config, so this function
+// fetches data on a per-user basis
 async function fetchArtistsForUser(
   username: string,
   thresholds: object,
@@ -24,11 +26,15 @@ async function fetchArtistsForUser(
     return fetchArtistsForPeriod(period, username, threshold)
   })
 
+  // get a single list of valid artists, removing any undefined items
   const lastFmData = flatten(await Promise.all(lastFmRequests))
+    .filter((item) => item)
 
   return lastFmData
 }
 
+// a user can have multiple listening period thresholds, so this
+// will fetch data for a given period
 async function fetchArtistsForPeriod(
   listeningPeriod: ListeningPeriod,
   username: string,
@@ -41,6 +47,8 @@ async function fetchArtistsForPeriod(
       return res.json()
     })
     .then((resJson: any) => {
+      validateApiResponse(resJson)
+
       const artistData: any = resJson.topartists.artist
       const filteredArtists = artistData.filter((artist) => artist.playcount >= threshold)
 
@@ -55,10 +63,14 @@ async function fetchArtistsForPeriod(
       })
     })
     .catch((err: any) => {
+      console.log('Caught an error trying to fetch artists')
       console.error(err)
+
+      return undefined
     })
 }
 
+// gets you an API url for a given listening period, and user
 function buildApiUrl(
   listeningPeriod: ListeningPeriod,
   username: string
@@ -66,9 +78,20 @@ function buildApiUrl(
   const apiKey = lastFmCredentials.key
   const url = `${BASE_URL}&limit=${RESULT_LIMIT}&api_key=${apiKey}&user=${username}&period=${listeningPeriod}`
 
-  console.log(`Using URL: ${url}`)
+  console.log(`Using LastFM URL: ${url}`)
 
   return url
+}
+
+function validateApiResponse(response) {
+  if (
+    !response ||
+    !response.topartists ||
+    !resJson.topartists.artist ||
+    resJson.topartists.artist === 0
+  ) {
+    throw new Error('Could not validate the Songkick API response')
+  }
 }
 
 export {
