c44bea6824368fe661e0fcb5dc373e069624818a
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-29 16:54:30 +0000

    Add ListeningPeriod enum

---
+++ src/api/LastFM/ListeningPeriods.ts
@@ -0,0 +1,10 @@
+enum ListeningPeriod {
+  ALL = 'overall',
+  ONE_WEEK = '7day',
+  ONE_MONTH = '1month',
+  THREE_MONTH = '3month',
+  SIX_MONTH = '6month',
+  ONE_YEAR = '12month',
+}
+
+export default ListeningPeriod
