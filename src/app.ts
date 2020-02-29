4bc5963a831eedf1f81b5581f45cf392a63f687b
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-29 23:55:21 +0000

    Update app.js to send concert data out once a week

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 // app.js
 import schedule from 'node-schedule'
 import MailSender from 'noodlesmail'
+import flatten from 'lodash.flatten'
+
 import { fetchArtistsForUser } from './api/LastFM'
 import { fetchArtistID, fetchConcertsForArtistId } from './api/Songkick'
-import flatten from 'lodash.flatten'
+import { filterLocation, filterConcerts } from './Utils'
 
 import gmailCredentials from './../credentials/GmailCredentials.json'
 import userConfig from './../config.json'
@@ -12,37 +14,29 @@ const mailClient = new MailSender(gmailCredentials)
 mailClient.setFrom('"ConcertFinder" <ryankrol.m@gmail.com>')
 mailClient.setTo('ryankrol.m@gmail.com')
 
-schedule.scheduleJob('0 0 1 * * *', async () => {
+schedule.scheduleJob('* * * * * 1', async () => {
   try {
-    mailClient.sendMail('Weekly Conert Report!', 'Body')
+    userConfig.forEach(async (user) => {
+      const lastFmData = (await fetchArtistsForUser(user.username, user.listeningPeriodThresholds))
+
+      const artistIdRequests = lastFmData.map((artistData) => {
+        return fetchArtistID(artistData.artist)
+      })
+
+      const artistIds = (await Promise.all(artistIdRequests)).filter((artistId) => artistId)
+
+      const concertRequests = artistIds.map((id) => {
+        return fetchConcertsForArtistId(id)
+      })
+
+      const concerts = flatten(await Promise.all(concertRequests))
+
+      const filteredConcerts = filterConcerts(filterLocation(user.locationData, concerts))
+
+      mailClient.sendMail('Weekly Conert Report!', JSON.stringify(filteredConcerts, null, 2))
+    })
   } catch(error) {
     console.log(error)
     mailClient.sendMail('Failed to generate concert report', error.toString())
   }
 })
-
-
-async function main() {
-  userConfig.forEach(async (user) => {
-    const lastFmData = (await fetchArtistsForUser(user.username, user.listeningPeriodThresholds)).slice(3,4)
-
-    const artistIdRequests = lastFmData.map((artistData) => {
-      return fetchArtistID(artistData.artist)
-    })
-
-    const artistIds = (await Promise.all(artistIdRequests)).filter((artistId) => artistId)
-
-    const concertRequests = artistIds.map((id) => {
-      return fetchConcertsForArtistId(id)
-    })
-
-    const concerts = flatten(await Promise.all(concertRequests))
-
-    console.log('amount of concerts')
-    console.log(concerts.length)
-    console.log(concerts)
-
-  })
-}
-
-main()
