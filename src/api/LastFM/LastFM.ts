e6991a82ee3e95d7cb4c785f0e546a7f18d95ad2
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-29 19:14:11 +0000

    Fill out library for fetching LastFM data

--- src/api/LastFM/LastFM.ts
+++ src/api/LastFM/LastFM.ts
@@ -1,15 +1,68 @@
+import fetch from 'node-fetch'
+import flatten from 'lodash.flatten'
+
+import { ListeningPeriod } from './'
 import lastFmCredentials from './../../../credentials/LastFMCredentials.json'
 
 const BASE_URL = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&format=json'
 const RESULT_LIMIT = 350
 
-async function fetchArtists(listeningPeriod, username) {
-  const url = buildApiUrl(listeningPeriod, username)
-
-  return {}
+type ArtistData = {
+  artist: string,
+  plays: Number,
+  url: string,
+  period: ListeningPeriod,
+  threshold: Number,
 }
 
-function buildApiUrl(listeningPeriod, username) {
+async function fetchArtistsForUser(
+  username: string,
+  thresholds: object,
+): Promise<ArtistData[]> {
+  const lastFmRequests = Object.keys(thresholds).map((period: ListeningPeriod) => {
+    const threshold = thresholds[period]
+    return fetchArtistsForPeriod(period, username, threshold)
+  })
+
+  const lastFmData = flatten(await Promise.all(lastFmRequests))
+
+  return lastFmData
+}
+
+async function fetchArtistsForPeriod(
+  listeningPeriod: ListeningPeriod,
+  username: string,
+  threshold: Number,
+): Promise<ArtistData[]> {
+  const url = buildApiUrl(listeningPeriod, username)
+
+  return fetch(url)
+    .then((res: any) => {
+      return res.json()
+    })
+    .then((resJson: any) => {
+      const artistData: any = resJson.topartists.artist
+      const filteredArtists = artistData.filter((artist) => artist.playcount >= threshold)
+
+      return filteredArtists.map((artist) => {
+        return {
+          artist: artist.name,
+          plays: artist.playcount,
+          url: artist.url,
+          period: listeningPeriod,
+          threshold: threshold,
+        }
+      })
+    })
+    .catch((err: any) => {
+      console.error(err)
+    })
+}
+
+function buildApiUrl(
+  listeningPeriod: ListeningPeriod,
+  username: string
+): string {
   const apiKey = lastFmCredentials.key
   const url = `${BASE_URL}&limit=${RESULT_LIMIT}&api_key=${apiKey}&user=${username}&period=${listeningPeriod}`
 
@@ -19,5 +72,5 @@ function buildApiUrl(listeningPeriod, username) {
 }
 
 export {
-  fetchArtists
+  fetchArtistsForUser
 }
