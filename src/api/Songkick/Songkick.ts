7a740d661ca030fc4d17f8d8ac86c121b347095a
Ryan Krol <ryankr@hotmail.co.uk>
2020-02-29 19:42:12 +0000

    Add API call to Songkick to fetch artist ID

---
+++ src/api/Songkick/Songkick.ts
@@ -0,0 +1,57 @@
+import fetch from 'node-fetch'
+import songkickCredentials from './../../../credentials/SongkickCredentials.json'
+
+const BASE_URL = "https://api.songkick.com/api/3.0/search/artists.json"
+
+type ArtistID = string
+
+async function fetchArtistId(
+  artistName: string
+): Promise<ArtistID> {
+  const url = buildApiUrl(artistName)
+
+  return fetch(url)
+    .then((res: any) => {
+      return res.json()
+    })
+    .then((resJson: any) => {
+      validateApiResponse(resJson)
+      return resJson.resultsPage.results.artist[0]
+    })
+    .catch((err: any) => {
+      console.log('Caught an error trying to get the artist ID')
+      console.error(err)
+
+      return undefined
+    })
+}
+
+function buildApiUrl(
+  artistName: string
+): string {
+  const apiKey = songkickCredentials.key
+  const uriArtistName = encodeURI(artistName)
+
+  const url = `${BASE_URL}?apikey=${apiKey}&query=${uriArtistName}`
+
+  console.log(`Using Songkick URL: ${url}`)
+
+  return url
+}
+
+function validateApiResponse(response) {
+  if (
+    !response ||
+    !response.resultsPage ||
+    !response.resultsPage.results ||
+    !response.resultsPage.results.artist ||
+    response.resultsPage.results.artist.length === 0 ||
+    response.resultsPage.status !== 'ok'
+  ) {
+    throw new Error('Could not validate the Songkick API response')
+  }
+}
+
+export {
+  fetchArtistId
+}
---
+++ src/api/Songkick/index.ts
@@ -0,0 +1,1 @@
+export { fetchArtistId } from './Songkick'
