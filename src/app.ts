eaf077e690298919f62481a6dbc38b4f2d21eae2
Ryan Krol <ryankr@hotmail.co.uk>
2020-07-15 21:53:15 +0100

    Update noodlemail dependency to noodle-email

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 // app.js
 import schedule from 'node-schedule'
-import MailSender from 'noodlemail'
+import MailSender from 'noodle-email'
 import flatten from 'lodash.flatten'
 
 import { fetchArtistsForUser } from './api/LastFM'
@@ -13,35 +13,48 @@ import userConfig from './../config.json'
 const mailClient = new MailSender(gmailCredentials)
 mailClient.setFrom('"ConcertFinder" <ryankrol.m@gmail.com>')
 
-schedule.scheduleJob({
-  hour: 15,
-  minute: 0,
-  dayOfWeek: 1
-}, async () => {
-  try {
-    userConfig.forEach(async (user) => {
-      const lastFmData = (await fetchArtistsForUser(user.username, user.listeningPeriodThresholds))
+schedule.scheduleJob(
+  {
+    hour: 15,
+    minute: 0,
+    dayOfWeek: 1
+  },
+  async () => {
+    try {
+      userConfig.forEach(async user => {
+        const lastFmData = await fetchArtistsForUser(
+          user.username,
+          user.listeningPeriodThresholds
+        )
 
-      const artistIdRequests = lastFmData.map((artistData) => {
-        return fetchArtistID(artistData.artist)
+        const artistIdRequests = lastFmData.map(artistData => {
+          return fetchArtistID(artistData.artist)
+        })
+
+        const artistIds = (await Promise.all(artistIdRequests)).filter(
+          artistId => artistId
+        )
+
+        const concertRequests = artistIds.map(id => {
+          return fetchConcertsForArtistId(id)
+        })
+
+        const concerts = flatten(await Promise.all(concertRequests))
+
+        const filteredConcerts = filterConcerts(
+          filterLocation(user.locationData, concerts)
+        )
+
+        mailClient.setTo(user.email)
+        mailClient.sendMail(
+          'Weekly Concert Report!',
+          JSON.stringify(filteredConcerts, null, 2)
+        )
       })
-
-      const artistIds = (await Promise.all(artistIdRequests)).filter((artistId) => artistId)
-
-      const concertRequests = artistIds.map((id) => {
-        return fetchConcertsForArtistId(id)
-      })
-
-      const concerts = flatten(await Promise.all(concertRequests))
-
-      const filteredConcerts = filterConcerts(filterLocation(user.locationData, concerts))
-
-      mailClient.setTo(user.email)
-      mailClient.sendMail('Weekly Concert Report!', JSON.stringify(filteredConcerts, null, 2))
-    })
-  } catch(error) {
-    console.log(error)
-    mailClient.setTo('ryankrol.m@gmail.com')
-    mailClient.sendMail('Failed to generate concert report', error.toString())
+    } catch (error) {
+      console.log(error)
+      mailClient.setTo('ryankrol.m@gmail.com')
+      mailClient.sendMail('Failed to generate concert report', error.toString())
+    }
   }
-})
+)
